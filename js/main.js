bf12eb56a69761ca0a12b5cf04e5f4e524c98db5
Marcio André <andremarcio1988@gmail.com>
2022-04-01 18:33:45 -0300

    fazendo javascrip da tela do preloader

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
+//Declarando Variáveis
 var btnContact = document.querySelector('.jl-btn-contact');
 
+//Page Preloader
+window.addEventListener('load', function () {
+    var pagePreloder = document.querySelector('.jl-preloader');
+    pagePreloder.classList.add('jl-fade-out');
+
+    setTimeout(function () {
+        pagePreloder.style.display = 'none';
+    }, 2000);
+});
+
+
+//Abrindo e Fechando Informações de Contato
 btnContact.addEventListener('click', function () {
     var boxContact = document.querySelector('.jl-contact-info');
     boxContact.classList.toggle('jl-is-open');
